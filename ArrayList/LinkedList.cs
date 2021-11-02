7b92416d93f01d673dd993c72d7eab8d92daa3a1
Breslavchik <elenabreslavskaya103@gmail.com>
2021-11-02 13:43:22 +0300

    LList started

---
+++ ArrayList/LinkedList.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    public class LinkedList
+    {
+        private Node _head;
+        private Node _tail;
+        public LinkedList()
+        {
+            _head = null;
+            _tail = null;
+        }
+        public LinkedList(int value)
+        {
+            _head = new Node(value);
+            _tail = _head;
+        }
+        public LinkedList(int[] array)
+        {
+            if (array.Length == 0)
+            {
+                _head = null;
+                _tail = null;
+            }
+            _head = new Node(array[0]);
+            _tail = _head;
+            for (int i=1;i<array.Length;i++)
+            {
+                _tail.Next = new Node(array[i]);
+                _tail = _tail.Next;
+            }
+        }
+        public int GetLength()
+        {            
+            Node current = _head;
+            int length = 1;
+            while (current.Next != null)
+            {
+                current = current.Next;
+                length++;
+            }
+            return length;
+
+        }
+        //public int this[int index]
+        //{
+        //    get
+        //    {
+        //        Node current = _head;
+        //        for (int i = 1; i < index; i++)
+        //        {
+        //            current = current.Next;
+        //        }
+        //        return current.Value;
+        //    }
+        //    set
+        //    {
+        //        Node current = _head;
+        //        for (int i = 1; i < index; i++)
+        //        {
+        //            current = current.Next;
+        //        }
+        //        current.Value = value;
+        //    }
+        //}
+        public int[] ToArray()
+        {
+            int length = GetLength();
+            int[] array = new int[length];
+            array[0] = _head.Value;
+            Node current = _head;
+            for (int i = 1; i < array.Length; i++)
+            {
+                current = current.Next;
+                array[i] = current.Value;
+            }
+            return array;
+        }
+        public void PrintArray(LinkedList list)
+        {
+            int [] array =list.ToArray();
+            for (int i = 1; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+        }
+        public void AddLast(int val)
+        {
+            _tail.Next = new Node(val);
+            _tail = _tail.Next;
+        }
+    }
+}
--- ArrayList/Program.cs
+++ ArrayList/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace ArrayList
+namespace Lists
 {
     class Program
     {
@@ -32,18 +32,22 @@ namespace ArrayList
             //ArrayList temp = new ArrayList(new int[] { 1, 2, 3 });
             ////act
             //temp.AddFirst(temp2);
-            
-            ArrayList temp2 = new ArrayList(new int[] { -10, 20, 50, 30, -8 });
-            ArrayList temp = new ArrayList(new int[] { 9, 1, 2, 3, 8, 9, 1 });
-            ArrayList temp3 = new ArrayList(new int[] { 3, 3, 3});
-            //act
-            // Console.WriteLine($"{temp.Sort()}"  );
-            temp3.PrintArray();
-            Console.WriteLine();
-            Console.WriteLine(temp3.GetLength());
-            
-            Console.WriteLine($"{temp3.RemoveAll(3)}");
-            temp3.PrintArray();
+
+            //ArrayList temp2 = new ArrayList(new int[] { -10, 20, 50, 30, -8 });
+            //ArrayList temp = new ArrayList(new int[] { 9, 1, 2, 3, 8, 9, 1 });
+            //ArrayList temp3 = new ArrayList(new int[] { 3, 3, 3});
+            ////act
+            //// Console.WriteLine($"{temp.Sort()}"  );
+            //temp3.PrintArray();
+            //Console.WriteLine();
+            //Console.WriteLine(temp3.GetLength());
+
+            //Console.WriteLine($"{temp3.RemoveAll(3)}");
+            //temp3.PrintArray();
+            LinkedList list = new LinkedList(new int[] { 1, 4, 6, 7, 8 });
+            Console.WriteLine(list.GetLength());
+            list.AddLast(19);
+            list.PrintArray(list);
 
         }
     }
